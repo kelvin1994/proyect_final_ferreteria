e2d70f301beefb53852a7099ec9ef4887e3dfb31
EVALUACION_KEL_G20 <kgzanabria@gmail.com>
2024-08-26 22:14:34 -0500

    mejorando carritox2

--- js/carrito/carrito.js
+++ js/carrito/carrito.js
@@ -38,11 +38,11 @@ function cargarProductosCarrito() {
                 </div>
                 <div class="carrito-producto-precio">
                     <small>Precio</small>
-                    <p>$${producto.precio}</p>
+                    <p>S/ ${producto.precio}</p>
                 </div>
                 <div class="carrito-producto-subtotal">
                     <small>Subtotal</small>
-                    <p>$${producto.precio * producto.cantidad}</p>
+                    <p>S/ ${producto.precio * producto.cantidad}</p>
                 </div>
                 <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
             `;
@@ -126,7 +126,7 @@ function vaciarCarrito() {
 
 function actualizarTotal() {
     const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
-    total.innerText = `$${totalCalculado}`;
+    total.innerText = `S/ ${totalCalculado}`;
 }
 
 botonComprar.addEventListener("click", comprarCarrito);
